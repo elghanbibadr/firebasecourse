a8a9e8948942fd43966b274023e676123d76228f
badr ghanbi <bghanbi50@gmail.com>
2023-05-08 15:03:09 +0100

    add the sign in componenet

--- src/componenet/Auth.jsx
+++ src/componenet/Auth.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
+import {auth} from "../config/firebase-config"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+
+
+
 
 const Auth = () => {
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+
+  const signIn=async()=>{
+   await createUserWithEmailAndPassword(auth,email,password)
+  }
   return (
     <div>
-        <input placeholder="email" />
-        <input placeholder="password" />
-        <button>submit</button>
+        <input placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+        <input placeholder="password" type="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+        <button onClick={signIn}>sign in </button>
     </div>
   )
 }
