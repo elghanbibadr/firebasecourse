1d5ad8aa3d9ba3f42dc02c893c45dd740cb9221d
badr ghanbi <bghanbi50@gmail.com>
2023-05-08 16:59:44 +0100

    this shit not working

--- src/componenet/Auth.jsx
+++ src/componenet/Auth.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
-import {auth} from "../config/firebase-config"
-import { createUserWithEmailAndPassword } from "firebase/auth"
-
+import {auth,googleProvider} from "../config/firebase-config"
+import { createUserWithEmailAndPassword,signInWithPopup } from "firebase/auth"
 
 
 
@@ -12,11 +11,17 @@ const Auth = () => {
   const signIn=async()=>{
    await createUserWithEmailAndPassword(auth,email,password)
   }
+  const signInWithGoogle=async()=>{
+   await signInWithPopup(auth,googleProvider)
+  }
+  
+
   return (
     <div>
         <input placeholder="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
         <input placeholder="password" type="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
         <button onClick={signIn}>sign in </button>
+        <button onClick={signInWithGoogle}>sign in with google </button>
     </div>
   )
 }
