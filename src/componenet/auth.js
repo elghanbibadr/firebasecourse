e1f7051bbaa6aa62bef206577f8538b4823c60ec
badr ghanbi <bghanbi50@gmail.com>
2023-05-08 13:56:51 +0100

    import firebase auth and insitilase a auth variable

---
+++ src/componenet/auth.js
@@ -0,0 +1,12 @@
+
+const Auth = () => {
+  return (
+    <div>
+        <input placeholder="email" />
+        <input placeholder="password" />
+        <button>submit</button>
+    </div>
+  )
+}
+
+export default Auth
